c23f92eb94e9ce8e51c7d061fefa782aa3a4cabf
H-oben <51274573+H-oben@users.noreply.github.com>
2021-12-05 15:59:02 -0600

    Updated source code

--- Source/prepImg.py
+++ Source/prepImg.py
@@ -1,4 +1,10 @@
-import header
+#Hunter Obendorfer
+#1834106
+#Python 3.7.11, OCV 3.4.2, MPL 3.4.3, numpy 1.21.2
+#use high resolution images, I used 4608 × 2592 images
+
+import cv2 as cv
+import numpy as np
 
 #prepare image
 def prepImg(img):
